b6349dc6d32f89c4355d8f3542c6a0734393793e
MinhNCFE <minhcongnguyen23102004@gmail.com>
2025-03-13 16:33:35 +0700

    Update TourDAO.java

--- Travel_Tours_WebApp/src/main/java/dao/TourDAO.java
+++ Travel_Tours_WebApp/src/main/java/dao/TourDAO.java
@@ -4,12 +4,39 @@
  */
 package dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import model.Tour;
 import util.DBContext;
 
 /**
  *
- * @author ngoth
+ * @author PC
  */
-public class TourDAO extends DBContext {
+public class TourDAO extends DBContext{
+
+    public TourDAO() {
+        super();
+    }
     
+    public Tour getById(int id) {
+        String sql = "select * from Tours where tour_id =?";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                String tour_name = rs.getString("tour_name");
+                String description = rs.getString("description");
+                long price = rs.getLong("price");
+                String destination = rs.getString("destination");
+                int seats = rs.getInt("available_seats");
+                Tour t = new Tour(id, tour_name, description, price, destination, seats);
+                return t;
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
 }
